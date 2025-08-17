6bee584314a30880af58eb640db54c26e25b992a
matt.shehata <matt.shehata@gmail.comgit config --global user.email matt.shehata@gmail.comgit config --global user.email matt.shehata@gmail.com>
2025-08-17 02:04:52 -0400

    fix: create static pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    output: "export", // tells Next.js to build a static site
+    images: {
+      unoptimized: true, // disables Next.js image optimization (needed for GitHub Pages)
+    },
+  };
+  
+  export default nextConfig;
+  
